4829d61031756b607d86bdf7bf0fe999eb028996
Stefano15d <1923010411@untels.edu.pe>
2023-05-22 16:11:37 -0500

    Daniel

---
+++ app/src/main/java/com/example/damep_jameshuanuco_danielventuri/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.damep_jameshuanuco_danielventuri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.button_enviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
+    }
+}
